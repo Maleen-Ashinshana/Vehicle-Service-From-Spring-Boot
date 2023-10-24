a8cede88be9cd0927e423229a2db30526bd23a04
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-25 03:17:27 +0530

    update

--- src/main/java/lk/ijse/gdse/dto/VehicleDTO.java
+++ src/main/java/lk/ijse/gdse/dto/VehicleDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.dto;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Pattern;
@@ -20,17 +21,26 @@ public class VehicleDTO {
     @NotNull(message = "Driver name cannot be empty")
     @Pattern(regexp = "[A-Za-z ]+")
     private String driver_name;
+    @NotNull(message = "Driver contact number cannot be empty")
+    private String driver_contact_number;
     @NotNull(message = "Vehicle Brand cannot be empty")
     private String vehicle_brand;
      @NotNull(message = "Vehicle Category cannot be empty")
     private String vehicle_category;
+    @NotNull(message = "Vehicle Type cannot be empty")
+    private String vehicle_type;
      @NotNull(message = "Fuel Type cannot be empty")
     private String fuel_type;
      @NotNull(message = "Fuel Usage cannot be empty")
     private String fuel_usage;
+    @NotNull(message = "Hybrid Type cannot be empty")
+    private String hybrid_or_no;
      @NotNull(message = "Seat Capacity cannot be empty")
     private String seat_capacity;
      @NotNull(message = "Transmission cannot be empty")
     private String transmission;
+
+
+    private String remark;
 
 }
--- src/main/java/lk/ijse/gdse/entity/VehicleEntity.java
+++ src/main/java/lk/ijse/gdse/entity/VehicleEntity.java
@@ -19,17 +19,26 @@ public class VehicleEntity implements SuperEntity{
     @Column(nullable = false)
     private String driver_name;
     @Column(nullable = false)
+    private String driver_contact_number;
+    @Column(nullable = false)
     private String vehicle_brand;
     @Column(nullable = false)
     private String vehicle_category;
+    @Column(nullable = false)
+    private String vehicle_type;
     @Column(nullable = false)
     private String fuel_type;
     @Column(nullable = false)
     private String fuel_usage;
     @Column(nullable = false)
+    private String hybrid_or_no;
+    @Column(nullable = false)
     private String seat_capacity;
     @Column(nullable = false)
     private String transmission;
+    @Column(nullable = false)
+    private String remark;
+
 
     @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<VehicleImageEntity> images;
